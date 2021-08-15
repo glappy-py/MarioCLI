a3e701c5657b019b895c9ecd74b9d0d58baf798b
GLAPPY <satwikv7@gmail.com>
2021-08-15 22:21:18 +0530

    bug fixes

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,48 +6,85 @@ namespace backend
 {
     class Program
     {
+        // Arg conditions
         static void Main(string[] args)
         {
-            if (args[0] == "addtodo"){
-                StreamReader todoreader = new StreamReader("G:\\codes - 2\\Mario\\todolist.txt");
-                List<string> todolist = new List<string>(todoreader.ReadToEnd().Split("\n"));
-                todoreader.Close();
-                StreamWriter todowriter = new StreamWriter("G:\\codes - 2\\Mario\\todolist.txt",true,Encoding.ASCII);
-                string todo = "";
-                foreach (string item in args)
-                {
-                    todo += " " + item;   
-                }
-                todo = todo.Replace(" addtodo todo ","");
-                todo = todolist.Count + ". " + todo + "\n";
-                todowriter.Write(todo);
-                todowriter.Close();
+            List<string> TempArgs = new List<string>(args);
+            string path = Directory.GetCurrentDirectory();
+            path = path.Remove(18);
+            string command = TempArgs[0];
+            TempArgs.RemoveAt(0);
+            string[] Args = TempArgs.ToArray();
+            if (command == "todo"){
+                addTodo(Args,path);
             }
-            if (args[0] == "removetodo"){
-                StreamReader todoreader = new StreamReader("G:\\codes - 2\\Mario\\todolist.txt");
-                List<string> todolist = new List<string>(todoreader.ReadToEnd().Split("\n"));
-                todoreader.Close();
-                StreamWriter todowriter = new StreamWriter("G:\\codes - 2\\Mario\\todolist.txt",false,Encoding.ASCII);
-                List<string> parsedArgs = new List<string>(args);
-                parsedArgs.Remove("removetodo");
-                foreach (string item in parsedArgs)
-                {
-                    Console.WriteLine(item);
-                }
-                foreach (string item in todolist.ToArray())
-                {
-                    Console.WriteLine(item);
-                    if (parsedArgs.Contains(item[0].ToString())){
-                        todolist.Remove(item);
-                    }
-                }
-                foreach (string item in todolist.ToArray())
-                {
-                    todowriter.WriteLine(item);
-                }
-                todowriter.Close();
+            if (command == "done"){
+                removeTodo(Args,path);
+            }
+            if (command == "list"){
+                listTodoTasks(Args,path);
             }
             
+        }
+        // Function for adding todo tasks
+        static void addTodo(string[] args,string path){
+            StreamReader todoreader = new StreamReader(path + @"\todolist.txt");
+            List<string> todolist = new List<string>(todoreader.ReadToEnd().Split("\n"));
+            todoreader.Close();
+            StreamWriter todowriter = new StreamWriter(path + @"\todolist.txt",true,Encoding.ASCII);
+            string todo = "";
+            foreach (string item in args)
+            {
+                todo += " " + item;   
+            }
+            todo = todo.Replace(" addtodo todo ","");
+            todo = todolist.Count + ". " + todo + "\n";
+            todowriter.Write(todo);
+            todowriter.Close();
+            Console.WriteLine();
+            foreach (string item in todolist)
+            {
+                if (item != ""){
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(todo);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        static void removeTodo(string[] args,string path){
+            StreamReader todoreader = new StreamReader(path + @"\todolist.txt");
+            List<string> todolist = new List<string>(todoreader.ReadToEnd().Split("\n"));
+            todoreader.Close();
+            StreamWriter todowriter = new StreamWriter(path + @"\todolist.txt",false,Encoding.ASCII);
+            List<string> parsedArgs = new List<string>(args);
+            parsedArgs.Remove("done");
+            parsedArgs.Remove("removetodo");
+            todolist.Remove("");
+            foreach (string item in todolist.ToArray())
+            {
+                if (parsedArgs.Contains(item[0].ToString())){
+                    todolist.Remove(item);
+                }
+            }
+            int counter = 1;
+            foreach (string item in todolist)
+            {
+                Console.WriteLine(item.Replace(item[0],Char.Parse(counter.ToString())));
+                counter++;
+            }
+            counter = 1;
+            foreach (string item in todolist)
+            {
+                todowriter.Write(item.Replace(item[0],Char.Parse(counter.ToString())));
+                counter++;
+            }
+            todowriter.Close();
+        }
+        static void listTodoTasks(string[] args,string path){
+            StreamReader todoreader = new StreamReader(path + @"\todolist.txt");
+            Console.Write(todoreader.ReadToEnd());
+            todoreader.Close();
         }
     }
 }
--- mario.bat
+++ mario.bat
@@ -1,76 +1,86 @@
 @echo off
 title Mario CLI
-if %1==join (
-    echo executing [92mzoom bot[0m
-    echo [90m[0m
-    cd /d "G:\codes - 2\local zoom bot\zoom_bot\"
-    zoom_bot.exe %2
-    @REM exit
-)
-@REM TODO: make a command to add more commands
-@REM TODO: make a gitbook about mario 
-@REM TODO: add tutorial text for making a new mario command
-@REM TODO: store the commands in .txt file in an encrypted form
-@REM TODO: make a command "mario about" which takes the user to marioCLI's gitbook
-if %1==list (
-    cd /d "%~dp0\"
-    FOR /F "tokens=* delims=" %%x in (todolist.txt) DO (
-        echo %%x
-    )
-)
-if %1==todo (
-    "%~dp0backend\bin\Debug\net5.0\backend.exe" addtodo %*
-)
-if %1==done (
-    "%~dp0backend\bin\Debug\net5.0\backend.exe" removetodo %*
-)
-if %1==terminate (
-    echo [91mshutting down system[0m
-    shutdown /s /t 1
-)
-if %1==start (
-    echo starting [92mVScode[0m at %CD%
-    code .
-    exit
-)
-if %1==migrate (
-    cd /d "G:\codes - 2"
-)
+@REM command for production build
+@REM "%~dp0backend\bin\Debug\net5.0\backend.exe" addtodo %*
+cd /d "%~dp0backend\"
+dotnet run %*
+@REM if %1==join (
+@REM     echo executing [92mzoom bot[0m
+@REM     echo [90m[0m
+@REM     cd /d "G:\codes - 2\local zoom bot\zoom_bot\"
+@REM     zoom_bot.exe %2
+@REM     @REM exit
+@REM )
+@REM @REM TODO: make a command to add more commands
+@REM @REM TODO: make a gitbook about mario 
+@REM @REM TODO: add tutorial text for making a new mario command
+@REM @REM TODO: store the commands in .txt file in an encrypted form
+@REM @REM TODO: make a command "mario about" which takes the user to marioCLI's gitbook
+@REM if %1==list (
+@REM     cd /d "%~dp0\"
+@REM     FOR /F "tokens=* delims=" %%x in (todolist.txt) DO (
+@REM         echo %%x
+@REM     )
+@REM )
+@REM if %1==todo (
+@REM     @REM command for production build
+@REM     @REM "%~dp0backend\bin\Debug\net5.0\backend.exe" addtodo %*
+@REM     cd /d "%~dp0backend\"
+@REM     dotnet run addtodo %*
+@REM )
+@REM if %1==done (
+@REM     @REM command for production build
+@REM     @REM "%~dp0backend\bin\Debug\net5.0\backend.exe" removetodo %*
+@REM     cd /d "%~dp0backend\"
+@REM     dotnet run removetodo %*
+@REM )
+@REM if %1==terminate (
+@REM     echo [91mshutting down system[0m
+@REM     shutdown /s /t 1
+@REM )
+@REM if %1==start (
+@REM     echo starting [92mVScode[0m at %CD%
+@REM     code .
+@REM     exit
+@REM )
+@REM if %1==migrate (
+@REM     cd /d "G:\codes - 2"
+@REM )
 
-if %1==help (
-    cd /d "G:\codes - 2\Mario\"
-    FOR /F "tokens=* delims=" %%x in (help.txt) DO (echo %%x)
-)
-if %1==node (
-    mkdir %2
-    cd %2
-    echo //Useful libraries > "index.js"
-    echo. >> "index.js"
-    echo. >> "index.js"
-    echo //Functions >> "index.js"
-    npm init -y
-    echo [92mnodejs project initialized, happy coding[0m
-    echo [90m[0m
-)
-if %1==react (
-    npx create-react-app %2
-    cd %2
-    echo [92mreact project initialized, happy coding ![0m
-    echo [90m[0m
-)
-if %1==install (
-    npm install %2 %3 %4 %5 %6 %7
-    echo [92mnpm package installed[0m
-    echo [90m[0m
-)
-if %1==make (
-    echo > %2
-    echo created [92m%2[0m at %CD%
-    echo [90m[0m
-)
-if %1==expo (
-    expo init %2 --template blank
-    cd %2
-    echo [92mexpo project initiliazed, happy coding ![0m
-    echo [90m[0m
-)
+@REM if %1==help (
+@REM     cd /d "G:\codes - 2\Mario\"
+@REM     FOR /F "tokens=* delims=" %%x in (help.txt) DO (echo %%x)
+@REM )
+@REM if %1==node (
+@REM     mkdir %2
+@REM     cd %2
+@REM     echo //Useful libraries > "index.js"
+@REM     echo. >> "index.js"
+@REM     echo. >> "index.js"
+@REM     echo //Functions >> "index.js"
+@REM     npm init -y
+@REM     echo [92mnodejs project initialized, happy coding[0m
+@REM     echo [90m[0m
+@REM )
+@REM if %1==react (
+@REM     npx create-react-app %2
+@REM     cd %2
+@REM     echo [92mreact project initialized, happy coding ![0m
+@REM     echo [90m[0m
+@REM )
+@REM if %1==install (
+@REM     npm install %2 %3 %4 %5 %6 %7
+@REM     echo [92mnpm package installed[0m
+@REM     echo [90m[0m
+@REM )
+@REM if %1==make (
+@REM     echo > %2
+@REM     echo created [92m%2[0m at %CD%
+@REM     echo [90m[0m
+@REM )
+@REM if %1==expo (
+@REM     expo init %2 --template blank
+@REM     cd %2
+@REM     echo [92mexpo project initiliazed, happy coding ![0m
+@REM     echo [90m[0m
+@REM )
